ab8c2772f1e56ae92c1fe8287bb0274d7b771d8c
Barys Tsibets <88795782+btsibets@users.noreply.github.com>
2021-11-17 16:14:40 -0800

    EditChannelReported script added

---
+++ Scripts/ScriptTest/EMSERecordEditChannelReported.js
@@ -0,0 +1,101 @@
+var debug ="";
+showDebug = true;
+showMessage = true;
+var useProductScript = true;
+var runEvent = false;
+var SCRIPT_VERSION = 3.0;
+servProvCode = aa.getServiceProviderCode();
+
+function getScriptText(vScriptName, servProvCode, useProductScript) {
+	vScriptName = vScriptName.toUpperCase();
+	var emseBiz = aa.proxyInvoker.newInstance(
+			"com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+	try {
+		if (useProductScript) {
+			var emseScript = emseBiz.getMasterScript(aa
+					.getServiceProviderCode(), vScriptName);
+		} else {
+			var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(),
+					vScriptName, "ADMIN");
+		}
+		return emseScript.getScriptText() + "";
+	} catch (err) {
+		return "";
+	}
+}
+eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", servProvCode, useProductScript));
+eval(getScriptText("INCLUDES_ACCELA_GLOBALS", servProvCode, useProductScript));
+//eval(getScriptText("INCLUDES_CUSTOM", servProvCode, useProductScript));
+var debug ="";
+showDebug = true;
+showMessage = true;
+var useProductScript = true;
+var runEvent = false;
+var SCRIPT_VERSION = 3.0;
+servProvCode = aa.getServiceProviderCode();
+
+function getScriptText(vScriptName, servProvCode, useProductScript) {
+	vScriptName = vScriptName.toUpperCase();
+	var emseBiz = aa.proxyInvoker.newInstance(
+			"com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+	try {
+		if (useProductScript) {
+			var emseScript = emseBiz.getMasterScript(aa
+					.getServiceProviderCode(), vScriptName);
+		} else {
+			var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(),
+					vScriptName, "ADMIN");
+		}
+		return emseScript.getScriptText() + "";
+	} catch (err) {
+		return "";
+	}
+}
+eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", servProvCode, useProductScript));
+eval(getScriptText("INCLUDES_ACCELA_GLOBALS", servProvCode, useProductScript));
+//eval(getScriptText("INCLUDES_CUSTOM", servProvCode, useProductScript));
+
+//var channel = "PHONE";
+var channel = aa.env.getValue("channel");
+
+var cap = aa.env.getValue("capId");
+var result = aa.cap.getCapID(cap);
+var capId = result.getOutput();
+
+var appCreateResult = editChannelReported(channel,capId);
+
+
+if (debug.indexOf("**ERROR") > 0)
+{
+aa.env.setValue("ScriptReturnCode", "1");
+aa.env.setValue("ScriptReturnMessage", debug);
+aa.env.setValue("isSuccess", "false");
+}
+else
+{
+aa.env.setValue("ScriptReturnCode", "0");
+aa.env.setValue("ScriptReturnMessage", message);
+aa.env.setValue("ScriptReturnMessage", debug);
+aa.env.setValue("isSuccess", "true");
+//aa.env.setValue("ScriptReturnMessage", appCreateResult);
+}
+var channel = "PHONE";                               // some records have empty drop down menu
+                                                     // need to check where to keep this data  to not hardcode it here
+var pCapId = aa.env.getValue("pCapId");
+var appCreateResult = editChannelReported(channel,capId);
+
+
+if (debug.indexOf("**ERROR") > 0)
+{
+aa.env.setValue("ScriptReturnCode", "1");
+aa.env.setValue("ScriptReturnMessage", debug);
+aa.env.setValue("isSuccess", "false");
+}
+else
+{
+aa.env.setValue("ScriptReturnCode", "0");
+aa.env.setValue("ScriptReturnMessage", message);
+aa.env.setValue("ScriptReturnMessage", debug);
+aa.env.setValue("isSuccess", "true");
+//aa.env.setValue("ScriptReturnMessage", appCreateResult);
+}
