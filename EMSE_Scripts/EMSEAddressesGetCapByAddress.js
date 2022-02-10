84e211f443aa61d593750deb4a1ee2f76c46de09
Barys Tsibets <88795782+btsibets@users.noreply.github.com>
2022-02-09 18:31:08 -0800

    APO and renewal scripts added

---
+++ EMSE_Scripts/EMSEAddressesGetCapByAddress.js
@@ -0,0 +1,56 @@
+var debug ="";
+showDebug = true;
+showMessage = true;
+var useProductScript = true;
+var runEvent = false;
+var SCRIPT_VERSION = 3.0;
+servProvCode = aa.getServiceProviderCode();
+
+function getScriptText(vScriptName, servProvCode, useProductScript) {
+	vScriptName = vScriptName.toUpperCase();
+	var emseBiz = aa.proxyInvoker.newInstance(
+			"com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+	try {
+		if (useProductScript) {
+			var emseScript = emseBiz.getMasterScript(aa
+					.getServiceProviderCode(), vScriptName);
+		} else {
+			var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(),
+					vScriptName, "ADMIN");
+		}
+		return emseScript.getScriptText() + "";
+	} catch (err) {
+		return "";
+	}
+}
+eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", servProvCode, useProductScript));
+eval(getScriptText("INCLUDES_ACCELA_GLOBALS", servProvCode, useProductScript));
+//eval(getScriptText("INCLUDES_CUSTOM", servProvCode, useProductScript));
+
+var ats= aa.env.getValue("ats");
+//var ats= "Building/Commercial/New/NA";
+
+
+//var cap = "COM-NEW-22-002901";
+var cap = aa.env.getValue("capId");
+var result = aa.cap.getCapID(cap);
+var capId = result.getOutput();
+
+var appCreateResult = getCapByAddress(ats,capId);
+
+
+if (debug.indexOf("**ERROR") > 0)
+{
+aa.env.setValue("ScriptReturnCode", "1");
+aa.env.setValue("ScriptReturnMessage", debug);
+aa.env.setValue("isSuccess", "false");
+}
+else
+{
+aa.env.setValue("ScriptReturnCode", "0");
+aa.env.setValue("ScriptReturnMessage", message);
+aa.env.setValue("ScriptReturnMessage", debug);
+aa.env.setValue("isSuccess", "true");
+//aa.env.setValue("ScriptReturnMessage", appCreateResult);
+
+}
